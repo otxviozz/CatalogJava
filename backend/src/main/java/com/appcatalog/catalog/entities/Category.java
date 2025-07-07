380b3ad468cbff299ed06ba9b275455e57c967ec
otavio <otaviotemoteo@gmail.com>
2025-07-07 09:41:27 -0300

    db annotations category

--- backend/src/main/java/com/appcatalog/catalog/entities/Category.java
+++ backend/src/main/java/com/appcatalog/catalog/entities/Category.java
@@ -3,9 +3,19 @@ package com.appcatalog.catalog.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_category") //já dá nome pra tabela
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY) // é o autoincrement do banco
 	private Long id;
 	private String name;
 	
