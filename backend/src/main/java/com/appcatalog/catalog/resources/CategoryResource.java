d269142cb7057193ea26338db05345daecb3ff5b
otavio <otaviotemoteo@gmail.com>
2025-07-11 16:14:37 -0300

    statuscode fix

--- backend/src/main/java/com/appcatalog/catalog/resources/CategoryResource.java
+++ backend/src/main/java/com/appcatalog/catalog/resources/CategoryResource.java
@@ -39,6 +39,6 @@ public class CategoryResource {
 	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
 		dto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
-		return ResponseEntity.ok().body(dto);
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
