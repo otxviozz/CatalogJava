7c7deef33470d1f57206e4bf3825395f3b03dcda
otavio <otaviotemoteo@gmail.com>
2025-07-22 10:35:59 -0300

    instant data

--- backend/src/main/java/com/appcatalog/catalog/entities/Category.java
+++ backend/src/main/java/com/appcatalog/catalog/entities/Category.java
@@ -1,12 +1,16 @@
 package com.appcatalog.catalog.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 
 @Entity
@@ -19,6 +23,30 @@ public class Category implements Serializable {
 	private Long id;
 	private String name;
 	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
+	
+	@PrePersist
+	public void prePersist( ) {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate( ) {
+		updatedAt = Instant.now();
+	}
+	
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+
 	public Category() {
 		
 	}
