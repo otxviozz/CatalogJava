a67b904cd2fa438c30a6212f2f8f700d7502452f
otavio <otaviotemoteo@gmail.com>
2025-07-22 09:04:52 -0300

    delete category

--- backend/src/main/java/com/appcatalog/catalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/appcatalog/catalog/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.appcatalog.catalog.services.exceptions.DatabaseException;
 import com.appcatalog.catalog.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -24,5 +25,17 @@ public class ResourceExceptionHandler {
 		err.setPath(request.getRequestURI());
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> entityNotFound(DatabaseException e, HttpServletRequest request){
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(HttpStatus.BAD_REQUEST.value());
+		err.setError("Database exception.");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 }
--- backend/src/main/java/com/appcatalog/catalog/services/CategoryService.java
+++ backend/src/main/java/com/appcatalog/catalog/services/CategoryService.java
@@ -5,12 +5,15 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.appcatalog.catalog.dto.CategoryDTO;
 import com.appcatalog.catalog.entities.Category;
 import com.appcatalog.catalog.repositories.CategoryRepository;
+import com.appcatalog.catalog.services.exceptions.DatabaseException;
 import com.appcatalog.catalog.services.exceptions.ResourceNotFoundException;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -54,6 +57,19 @@ public class CategoryService {
 		return new CategoryDTO(entity);
 		} catch (EntityNotFoundException e){
 			throw new ResourceNotFoundException("Id not found "+id);
+		}
+	}
+
+	@Transactional(propagation = Propagation.SUPPORTS)
+	public void delete(Long id) {
+		if(!repository.existsById(id)) {
+			throw new ResourceNotFoundException("Id not found.");
+		}
+		try {
+			repository.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation.");
 		}
 	}
 }
