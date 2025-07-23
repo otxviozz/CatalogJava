b51efceebcc4461108a953becccd9ef67c4c796e
otavio <otaviotemoteo@gmail.com>
2025-07-23 14:19:51 -0300

    creating jointable

--- backend/src/main/java/com/appcatalog/catalog/entities/Product.java
+++ backend/src/main/java/com/appcatalog/catalog/entities/Product.java
@@ -11,6 +11,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -29,6 +32,10 @@ public class Product implements Serializable {
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant date;
 	
+	@ManyToMany
+	@JoinTable(name="tb_product_category",
+				joinColumns = @JoinColumn(name= "product_id"),
+				inverseJoinColumns = @JoinColumn(name= "category_id"))
 	Set<Category> categories = new HashSet<>();
 	
 	public Product() {
