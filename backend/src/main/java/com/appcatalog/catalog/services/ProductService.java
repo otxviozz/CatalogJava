94627986cbc574f3892472a936996839020507d6
otavio <otaviotemoteo@gmail.com>
2025-07-23 14:58:46 -0300

    product category

--- backend/src/main/java/com/appcatalog/catalog/services/ProductService.java
+++ backend/src/main/java/com/appcatalog/catalog/services/ProductService.java
@@ -36,7 +36,7 @@ public class ProductService {
 	public ProductDTO findById(Long id) {
 		Optional<Product> obj = repository.findById(id);
 		Product entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
-		return new ProductDTO(entity);
+		return new ProductDTO(entity, entity.getCategories());
 	}
 
 	@Transactional
