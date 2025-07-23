34a5c23529ae19e03cd06264d7a53c7959c2bde7
otavio <otaviotemoteo@gmail.com>
2025-07-23 16:28:18 -0300

    insert and update category

--- backend/src/main/java/com/appcatalog/catalog/services/ProductService.java
+++ backend/src/main/java/com/appcatalog/catalog/services/ProductService.java
@@ -55,7 +55,7 @@ public class ProductService {
 	public ProductDTO update(Long id, ProductDTO dto) {
 		try {
 			Product entity = repository.getReferenceById(id);
-			//entity.setName(dto.getName());
+			copyDtoToEntity(dto,entity);
 			entity = repository.save(entity);
 			return new ProductDTO(entity);
 		} catch (EntityNotFoundException e){
